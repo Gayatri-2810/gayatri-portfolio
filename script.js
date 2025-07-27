0eb58d38dafd0d2111cf0ab3b57bb76f028f4079
Gayatri-2810 <gpdeshmukh2004@gmail.com>
2025-07-28 00:15:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -5,4 +6,12 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
+});
+
